db801d56abbd27a7fd8a076dccb1870ebc96c8cf
Diego Volpatto <diego@esss.co>
2019-07-16 11:52:02 -0300

    WIP

--- python_package/bindings/bindings.cpp
+++ python_package/bindings/bindings.cpp
@@ -1,10 +1,10 @@
 #include <pybind11/pybind11.h>
-#include <example/example_cpp.hpp>
+#include <example_cpp.hpp>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(example, m) {
 m.doc() = "pybind11 python_package plugin"; // optional module docstring
 
-m.def("add", &add, "A function which adds two numbers");
+m.def("add", &example_cpp::add, "A function which adds two numbers");
 }
